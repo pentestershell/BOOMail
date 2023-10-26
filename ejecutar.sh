33855570aa8024beb9ec2e131ef8a9a653cdca60
pentestershell <73437773+pentestershell@users.noreply.github.com>
2023-10-26 19:43:48 +0200

    Create ejecutar.sh

---
+++ ejecutar.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+while true
+do
+    # Tu comando para ejecutar el script de Python
+    python3 script.py
+
+    # Esperar 1 segundos antes de ejecutar nuevamente
+    sleep 1
+done
